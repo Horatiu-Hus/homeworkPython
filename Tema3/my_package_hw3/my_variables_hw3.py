6edce7fd729a9fcb4e3174a0632a7bef51fbb968
Horatiu-Hus <112974677+Horatiu-Hus@users.noreply.github.com>
2022-09-22 16:14:12 +0300

    Tema 3

--- Tema3/my_package_hw3/my_variables_hw3.py
+++
@@ -1,3 +0,0 @@
-a = input('Introduceti un numar intreg de la tastatura:')
-n = 0
-vef = True
---
+++ Tema3/my_script_hw_three.py
@@ -0,0 +1,4 @@
+from Tema3.my_package_hw3.my_variables_hw_three import n
+from Tema3.my_package_hw3.my_functions_hw_three import f1
+
+f1(1)
